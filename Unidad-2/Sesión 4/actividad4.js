e4ca3afc932616ae306d8985bd1ebf8dedde2eab
Angela Bernabe <angbergon@alu.edu.gva.es>
2023-10-03 17:31:14 +0200

    Sesiones 3-5

---
+++ Unidad-2/Sesión 4/actividad4.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let num = prompt("Ponme un número:");
+let res = !isNaN(num) ? alert("El valor introducido es un número.") :
+alert("El valor introducido no es un número.");
---
+++ Unidad-2/Sesión 5/actividad5.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let nom = prompt("Introduzca su nombre:");
+let ape = prompt("Introduzca sus apellidos:");
+let sala = parseFloat(prompt("Introduzca su salario:"));
+let edad = parseInt(prompt("Introduzca su edad:"));
+
+if(sala <= 2000)
+{
+    if(sala >= 1000 && sala <= 2000)
+    {
+        if(edad > 45)
+        {
+            sala = sala + (sala * 0.03);
+        }
+        else
+        {
+            sala = sala + (sala * 0.1);
+        }
+    }
+    else
+    {
+        if(edad < 30)
+        {
+            sala = 1100;
+        }
+        else if(edad >= 30 && edad < 45)
+        {
+            sala = sala + (sala * 0.03);
+        }
+        else
+        {
+            sala = sala + (sala * 0.15);
+        }
+    }
+}
+
+alert("Usuario con nombre completo: " + nom + " " + ape + "\nCuya edad es: " + edad + "\nSu salario final es: " + sala );
